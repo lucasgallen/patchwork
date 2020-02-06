a1dd01bde4f2cde6e2f584f79349a13209abe4f5
Lucas Allen <lucas.g.allen@gmail.com>
2020-02-06 17:40:58 +0200

    Update config files for auth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/sign_in' => 'devise/session#new'
+    get '/sign_out' => 'devise/session#destroy'
+  end
+
   get 'home/show'
   get 'about', to: 'about#show'
 
   root 'home#show'
+
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#show'
+
+    root 'dashboard#show'
+  end
 end
