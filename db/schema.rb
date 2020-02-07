709db39c0bae02e999b27ecc9bc80fa3f74f5a53
Lucas Allen <lucas.g.allen@gmail.com>
2020-02-07 15:14:05 +0200

    Add migration to create articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_132746) do
+ActiveRecord::Schema.define(version: 2020_02_06_154928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "author", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "role", default: "", null: false
