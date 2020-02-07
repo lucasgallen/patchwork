babc177ba8665b4d0168ba6b6c211341a5c36ec1
Lucas Allen <lucas.g.allen@gmail.com>
2020-02-07 15:33:15 +0200

    Use access denied catch in Application controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,13 +2,4 @@ class Admin::BaseController < ApplicationController
   check_authorization
 
   layout 'admin'
-
-  rescue_from CanCan::AccessDenied do |exception|
-    if user_signed_in?
-      flash[:alert] = exception.message
-      redirect_to root_path
-    else
-      redirect_to user_session_path
-    end
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  rescue_from CanCan::AccessDenied do |exception|
+    if user_signed_in?
+      flash[:alert] = exception.message
+      redirect_to root_path
+    else
+      redirect_to user_session_path
+    end
+  end
+
   protected
 
   def after_sign_in_path_for(user)
