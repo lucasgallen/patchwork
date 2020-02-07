14da2813f6a44325832287bfdd108c38e5e11e58
Lucas Allen <lucas.g.allen@gmail.com>
2020-02-07 18:50:01 +0200

    Add routable locale functionality (first pass)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     if user_signed_in?
@@ -15,5 +16,17 @@ class ApplicationController < ActionController::Base
     default_path = user.role == 'admin' ? admin_root_path : root_path
 
     return stored_location_for(user) || default_path
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    if I18n.default_locale != I18n.locale
+      {locale: I18n.locale}.merge options
+    else
+      {locale: nil}.merge options
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    get '/sign_in' => 'devise/session#new'
-    get '/sign_out' => 'devise/session#destroy'
-  end
+  scope "(:locale)", locale: /en/ do
+    devise_for :users
+    devise_scope :user do
+      get '/sign_in' => 'devise/session#new'
+      get '/sign_out' => 'devise/session#destroy'
+    end
 
-  get 'home/show'
-  get 'about', to: 'about#show'
+    get 'home/show'
+    get 'about', to: 'about#show'
 
-  root 'home#show'
+    root 'home#show'
 
-  resources :articles, only: [:show, :index]
-  scope '/admin' do
-    resources :articles, as: 'admin_articles'
-  end
+    resources :articles, only: [:show, :index]
+    scope '/admin' do
+      resources :articles, as: 'admin_articles'
+    end
 
-  namespace :admin do
-    get 'dashboard', to: 'dashboard#show'
+    namespace :admin do
+      get 'dashboard', to: 'dashboard#show'
 
-    root 'dashboard#show'
+      root 'dashboard#show'
+    end
   end
 end
