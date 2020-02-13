00c6ad9bad3d5fe91e40802bbc5d5ec963ab116f
Lucas Allen <lucas.g.allen@gmail.com>
2020-02-13 17:29:00 +0200

    Update localization and refactor directory.

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Patchwork
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
     config.i18n.default_locale = :tr
   end
 end
