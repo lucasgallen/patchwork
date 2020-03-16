9c7c975cb5c3c3c2eabcd67a88230344b3293dc1
Lucas Allen <lucas.g.allen@gmail.com>
2020-03-16 16:04:25 +0200

    Let user add/edit primary image for products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -56,7 +56,7 @@ class ProductsController < ApplicationController
   protected
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :currency)
+    params.require(:product).permit(:name, :description, :price, :currency, :primary_image)
   end
 
   def product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  has_one_attached :primary_image
 end
