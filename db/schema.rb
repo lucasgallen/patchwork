44a5d7b6c220de7c8d3060cab5c248583156d6a7
Lucas Allen <lucas.g.allen@gmail.com>
2020-03-30 16:50:19 +0300

    Add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_131034) do
+ActiveRecord::Schema.define(version: 2020_03_25_132932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2020_03_25_131034) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "slug", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
   create_table "products", force: :cascade do |t|
