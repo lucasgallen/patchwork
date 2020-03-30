f5899f05179fc23a5f40b97873ee24dcf86be050
Lucas Allen <lucas.g.allen@gmail.com>
2020-03-30 16:44:42 +0300

    Install jQuery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': "tether",
+  })
+)
+
+const envConfig = module.exports = environment
+const aliasConfig = module.exports = {
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery'
+    }
+  }
+}
+
+module.exports = merge(envConfig.toWebpackConfig(), aliasConfig)
