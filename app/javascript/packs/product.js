4eb47cfe759375c80831a08759dbfe15e8f30b3c
Lucas Allen <lucas.g.allen@gmail.com>
2020-04-01 11:03:14 +0300

    Fix product carousel by moving fnality into JS

---
+++ app/javascript/packs/product.js
@@ -0,0 +1,23 @@
+require("bootstrap")
+
+class Product {
+  constructor() {
+    this.$carousel = $('#product-carousel');
+    this.$carouselIndicators = $('#carousel-indicators');
+
+    this.init();
+  }
+
+  init() {
+    this.$carousel.carousel({ interval: false });
+    this.$carousel.on('slide.bs.carousel', e => {
+      const $nextIndicator = this.$carouselIndicators.find(`[data-slide-to="${e.to}"]`);
+      const $activeIndicator = this.$carouselIndicators.find('.active');
+
+      $activeIndicator.toggleClass('active');
+      $nextIndicator.toggleClass('active');
+    });
+  }
+}
+
+new Product();
