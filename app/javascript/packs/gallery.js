a927f1af27f09a085c99132dff81e08d4e48fcbd
Lucas Allen <lucas.g.allen@gmail.com>
2020-04-01 11:00:21 +0300

    Build inifinite scroll for gallery page.

--- app/javascript/packs/gallery.js
+++ app/javascript/packs/gallery.js
@@ -1,17 +1,42 @@
+require('lazyload/lazyload');
+
 class Gallery {
   constructor() {
+    this.INF_SCROLL_THRESHOLD_PX = 250;
+    this.TIMEOUT_MS = 300;
+
+    this.$window = $(window);
     this.$infScroll = $('#infinite-scroll-container');
+    this.baseURL = this.$infScroll.data('url');
+
     this.currentPage = 0;
+    this.canExScroll = true;
+    this.loadingPage = false;
 
     this.init();
   }
 
   init() {
-    // hookup scroll event to detect bottom of page
-    // is at bottom of infScroll?
-      // yes
-        // -> paginateGallery
-        // -> this.$infScroll.data('current-page', this.currentPage)
+    this.setupInfScroll();
+
+    this.$infScroll.on('gallery:load', () => {
+      $('.lazyload').lazyload();
+    });
+  }
+
+  setupInfScroll() {
+    if (this.$infScroll.data('last-page')) return;
+
+    if (this.nearContainerBottom()) {
+      this.loadingPage = true;
+      this.paginateGallery();
+    }
+
+    this.$window.on('scroll', e => {
+      if (this.loadingPage) return;
+
+      this.throttledInfScroll(e);
+    });
   }
 
   galleryPages() {
@@ -19,17 +44,50 @@ class Gallery {
   }
 
   paginateGallery() {
-    // load next page
-    // this.currentPage += 1
-    // append results
-    // is last page?
-      // -> append end of results markup
-      // -> disconnect infinite scroll detection
+    const pageURL = `${this.baseURL}/${this.currentPage + 1}`;
+
+    $.ajax({
+      method: 'get',
+      url: pageURL,
+      dataType: 'json'
+    }).done(data => {
+      this.addGalleryPage(data);
+      this.currentPage += 1;
+      this.loadingPage = false;
+    }).fail(err => {
+      // TODO: setup error handling
+      console.log(err);
+    });
+  }
+
+  addGalleryPage(data) {
+    this.$infScroll.append(data.html);
+    this.$infScroll.trigger('gallery:load');
+
+    if (data.last) {
+      this.$window.off('scroll');
+    }
+  }
+
+  throttledInfScroll(e) {
+    if (!this.canExScroll) return;
+
+    this.canExScroll = false;
+    if (this.nearContainerBottom()) {
+      this.loadingPage = true;
+      this.paginateGallery();
+    }
+
+    setTimeout(() => {
+      this.canExScroll = true;
+    }, this.TIMEOUT_MS);
   }
 
   nearContainerBottom() {
-    // magic here
-    // return true or false
+    const containerHeight = this.$infScroll.outerHeight();
+    const bottomWindowPos = this.$window.scrollTop() + this.$window.innerHeight();
+
+    return containerHeight - bottomWindowPos < this.INF_SCROLL_THRESHOLD_PX;
   }
 }
 
