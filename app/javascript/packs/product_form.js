6ab23b12a8dce31a494db0fe7522152c34cee88a
Lucas Allen <lucas.g.allen@gmail.com>
2020-05-15 16:43:13 +0300

    Implement direct upload progress bar for admin product form

--- app/javascript/packs/product_form.js
+++ app/javascript/packs/product_form.js
@@ -2,7 +2,30 @@ require("trix")
 require("bootstrap/js/dist/dropdown");
 require("bootstrap-select");
 
+import { DirectUpload } from '@rails/activestorage';
+
 $.fn.selectpicker.Constructor.BootstrapVersion = '4.4.1';
+
+const UPLOAD_PROGRESS_HTML = (() => {
+  const $container = $('<div>', { class: 'col-md-6 ml-3 mb-2 ' });
+  const $progress = $('<div>', { class: 'progress col-8 p-0 ml-4' });
+  const $label = $('<span>', { class: 'progress-file-name col-12' });
+  const $percent = $('<span>', { class: 'progress-percent' });
+  const $bar = $('<div>', {
+      'aria-valuenow': 0,
+      'aria-valuemin': 0,
+      'aria-valuemax': 100,
+      class: 'progress-bar progress-bar-striped progress-bar-animated flex-row',
+      role: 'progressbar',
+      text: '%',
+    });
+  $bar.prepend($percent);
+  $progress.append($bar);
+  $container.append($label);
+  $container.append($progress);
+
+  return $container;
+})();
 
 class ProductForm {
   constructor() {
@@ -12,6 +35,8 @@ class ProductForm {
     this.$detailImages = $('#detail-images');
     this.$detailImagesPreview = $('#detail-images-preview');
     this.$detailImage = $('<img/>', { class: 'w-25 mr-2 mb-2' });
+    this.$form = $('#product-form');
+    this.$fileInputs = $('input[type="file"]');
 
     this.init();
   }
@@ -26,6 +51,25 @@ class ProductForm {
     this.$detailImages.change(event => {
       this.$detailImagesPreview.empty();
       this.prevDetailImages(event.currentTarget);
+    });
+
+    this.setupDirectUploads();
+  }
+
+  setupDirectUploads() {
+    this.$form.on('direct-upload:start', 'input[type="file"]', e => {
+      const $prog = $(UPLOAD_PROGRESS_HTML).clone();
+      $prog.attr('data-file-id', e.detail.id);
+      $prog.find('.progress-file-name').text(e.detail.file.name);
+      $prog.find('.progress-percent').text('0');
+      $('#image-uploads').append($prog);
+    });
+
+    this.$form.on('direct-upload:progress', 'input[type="file"]', e => {
+      const $progPercent = $(`[data-file-id="${e.detail.id}"] .progress-percent`);
+      const intProgress = e.detail.progress | 0;
+      $progPercent.text(intProgress);
+      $progPercent.parent().attr('style', `width:${intProgress}%;`);
     });
   }
 
