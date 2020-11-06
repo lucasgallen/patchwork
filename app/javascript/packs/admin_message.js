6f5b7a8a04cc9db953eb8529061df53196a0e1eb
Lucas Allen <lucas.g.allen@gmail.com>
2020-11-06 12:49:38 +0000

    Let user archive messages via admin messages show page

--- app/javascript/packs/admin_message.js
+++ app/javascript/packs/admin_message.js
@@ -1,29 +1,38 @@
 class AdminMessage {
   constructor() {
     this.$markRepliedButton = $('#message-replied-button');
+    this.$deleteButton = $('#message-delete-button');
+
     this.init();
   }
 
   reset() {
     this.$markRepliedButton.off('click');
+    this.$deleteButton.off('click');
   }
 
   init() {
-    this.$markRepliedButton.on('click', e => {
-      const $el = $(e.currentTarget);
-      const url = $el.data('url');
-      const confirmation = window.confirm($el.data('confirm-message'));
-      if (!confirmation) return;
+    this.reset();
 
-      $.ajax({
-        method: 'put',
-        url: url,
-        headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
-        success: response => {
-          Turbolinks.visit(window.location.href, { action: 'replace' });
-        },
-        error: err => console.log(err)
-      });
+    this.$deleteButton.on('click', e => this.updateMessage(e));
+    this.$markRepliedButton.on('click', e => this.updateMessage(e));
+  }
+
+  updateMessage(e) {
+    const $el = $(e.currentTarget);
+    const url = $el.data('url');
+    const successLocation = $el.data('goto') || window.location.href;
+    const confirmation = window.confirm($el.data('confirm-message'));
+    if (!confirmation) return;
+
+    $.ajax({
+      method: 'put',
+      url: url,
+      headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+      success: response => {
+        Turbolinks.visit(successLocation, { action: 'replace' });
+      },
+      error: err => console.log(err)
     });
   }
 }
